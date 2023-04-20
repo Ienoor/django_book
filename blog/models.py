4d4faafa002b818215d78ad33c6b940d9609f4bd
Vinogradov Artem <vinogradov44@outlook.com>
2023-04-20 08:49:04 +0300

    submission of post and detailed post

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -16,6 +20,9 @@ class Post(models.Model):
     created = models.DateField(auto_now_add=False)
     updated = models.DateField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         verbose_name = "Post"
